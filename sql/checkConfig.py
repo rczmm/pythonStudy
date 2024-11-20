d0122ad451f83efb4c89e54cfcae48ce3d8713b4
rcz <3179623450@qq.com>
2024-11-20 15:23:01 +0800

    feat(sql): 添加检查配置数据处理脚本
    - 新增 checkConfig.py 脚本，用于处理配置数据并生成 SQL 语句
    - 添加 config.csv 文件，包含检查配置数据
    - 更新 track_coord

---
+++ sql/checkConfig.py
@@ -0,0 +1,74 @@
+﻿import csv
+
+configs = []
+
+with open('config.csv', 'r', encoding='utf-8') as f:
+    config = csv.reader(f)
+    for row in config:
+        data = row[0].split(" ")
+        tenant_id = data[2]
+        name = data[5]
+        level = data[8]
+        sc_type = data[9]
+        common = data[10]
+        photo_flag = data[11]
+        photo_num = data[12]
+        inhabitant_flag = data[13]
+        advise = data[14]
+        process_time = data[16]
+        config = {
+            "name": name,
+            "level": level,
+            "sc_type": sc_type,
+            "common": common,
+            "photo_flag": photo_flag,
+            "photo_num": photo_num,
+            "flag": inhabitant_flag,
+            "advise": advise,
+            "process_time": process_time,
+            "pid": 0
+
+        }
+        configs.append(config)
+
+sqls = []
+
+commons = set()
+
+for config in configs:
+    commons.add(config['common'])
+
+sqls = []
+
+for config in configs:
+    pids = {
+        '燃气软管': 168,
+        '表后管道': 169,
+        '燃气阀门': 170,
+        '户内燃气管道': 171,
+        '燃气表': 172,
+        '表前管道': 173,
+        '其他': 174,
+        '燃气设备': 175,
+    }
+    sql = f'''
+    INSERT into "JHC-MH-SC"."check_config"("tenant_id","name","sc_level","sc_type","common","photo_flag","photo_num","inhabitant_flag","sc_advise","sc_process_time","pid","order_R")
+VALUES ('jytbrq','{config['name']}','{config['level']}','{config['sc_type']}','{config['common']}',{config['photo_flag']},{config['photo_num']},'{config['flag']}','{config['advise']}','{config['process_time']}','{pids[config['common']]}',0);'''
+    sqls.append(sql)
+
+com = []
+
+for common in commons:
+    sql = f'''INSERT INTO "JHC-MH-SC"."check_config"("tenant_id","common","photo_flag","photo_num","inhabitant_flag","pid","order_R")
+VALUES('jytbrq','{common}','0','0','居民','0',0)
+                '''
+    com.append(sql)
+
+for i in com:
+    print(i)
+
+for i in sqls:
+    print(i)
+
+if __name__ == '__main__':
+    pass
--- test/track_coord.py
+++ test/track_coord.py
@@ -8,7 +8,7 @@ from script.importCenterLine import direction_lite, extract_waypoints
 
 url = "http://110.185.163.49:22222/stage-api/ins/app/listTasksApp"
 
-authorization = """Bearer eyJhbGciOiJIUzUxMiJ9.eyJ0ZW5hbnRfaWQiOiI0NDIxMSIsIm9zIjoiQ2hyb21lIiwidXNlcl9pZCI6MjEwLCJ1c2VyX2tleSI6IjBkOGIyZjI2LTk2NTEtNGQwMy04MDI5LWIyMWNkYmMwZTU3OCIsImRlcHRfbmFtZSI6IjQ0MjExIiwiZGVwdF9pZCI6NDE5LCJ1c2VybmFtZSI6InJjeiJ9.VIQBGTD0IgSLj4DzENxzmh4VuBeHrewBiwP55CDXpTptVL2VTZyYRbdT24xBjQ8yvaSvROuM1j7bVkqZPJPwPA"""
+authorization = """Bearer eyJhbGciOiJIUzUxMiJ9.eyJ0ZW5hbnRfaWQiOiI0NDIxMSIsIm9zIjoiQ2hyb21lIiwidXNlcl9pZCI6MjA5LCJ1c2VyX2tleSI6IjdjMDIzNTYzLWM4OWYtNDU0ZC05MGZlLWQ1NTE5YTdmZDA3MSIsImRlcHRfbmFtZSI6IjQ0MjExIiwiZGVwdF9pZCI6NDE5LCJ1c2VybmFtZSI6ImxuIn0.3msnhzu6NVcV2Cis5f4fuLT_A-GEkaYJwYwlNvP1prp4-nHYiBAd7cDVi_ybnpM3-6OZJnm4axpIDnYZpiYobQ"""
 
 headers = {
     'authorization': authorization,
@@ -28,10 +28,9 @@ def get_coords():
     # destination = input("请输入终点坐标(经度在前，纬度在后): ")
     # waypoints = input("请输入途经点坐标(经度在前，纬度在后，坐标点用|英文竖线隔开): ")
 
-    origin = """104.091405,29.674315"""
-    destination = """104.070398,29.614482"""
-    waypoints = """104.089317,29.672248|104.081744,29.673503|104.078313,29.671034|104.06959,29.671289|104.068099,29.67873|104.070753,29.670351|104.068813,29.666428|104.063136,29.667652|104.062417,29.66624|104.060225,29.657547"""
-
+    origin = """104.091373,29.674351"""
+    destination = """104.070878,29.654352"""
+    waypoints = """104.089423,29.672201|104.083306,29.673425|104.081374,29.673205|104.081357,29.670997|104.069652,29.671193|104.067554,29.666827|104.072459,29.662873|104.077525,29.662841|104.077903,29.654618|"""
     # 百度路径规划
     result = direction_lite(origin, destination, waypoints)
 
@@ -49,7 +48,7 @@ if __name__ == '__main__':
     response = requests.post(url, headers=headers, data=json.dumps(request_data))
     if response.status_code == 200:
         taskList = response.json()
-        taskId = taskList['data']['records'][0]['taskId']
+        taskId = taskList['data']['records'][1]['taskId']
         track_url = f'''http://110.185.163.49:22222/stage-api/ins/app/uploadTaskTrace'''
         points = get_coords()
         coords = []
@@ -61,6 +60,7 @@ if __name__ == '__main__':
                 "lng": point['lng'],
                 "time": get_current_time_plus_two_seconds()
             }
+            print("轨迹点加1,{}".format(coord))
             coords.append(coord)
             i += 1
 
