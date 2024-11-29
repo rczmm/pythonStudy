d82cd9731ce525bbc3b9c6f6101f390895e506eb
rcz <3179623450@qq.com>
2024-11-29 14:59:34 +0800

    refactor(script): 调整网页刷新间隔和请求 URL
    - 将 refresh_web_page 函数的默认间隔时间从 0.5 秒调整为 0.3 秒
    - 更新 get 函数中的 URL 和 cookie 信息
    - 新增对响应内容的判断和打印

--- script/demo.py
+++ script/demo.py
@@ -14,7 +14,7 @@ def is_chrome_window():
         return False
 
 
-def refresh_web_page(interval=0.5):
+def refresh_web_page(interval=0.3):
     """ 每间隔一定的秒数刷新一次网页（默认5秒），但只在Chrome浏览器下执行 """
     while True:
         if is_chrome_window():
@@ -25,11 +25,11 @@ def refresh_web_page(interval=0.5):
         # 等待一定的时间间隔
         time.sleep(interval)
 
-
+cookie_ln = """lang=zh-cn; device=desktop; keepLogin=on; hideMenu=false; preProductID=1; theme=blue; lastProject=1; docSpaceParam=%7B%22type%22%3A%22project%22%2C%22objectID%22%3A%221%22%2C%22libID%22%3A%220%22%2C%22moduleID%22%3A%220%22%2C%22browseType%22%3A%22all%22%2C%22param%22%3A%220%22%7D; preBranch=0; vision=rnd; zentaosid=4b7b9938eb3ba3f0beeb707358495713; za=luona; zp=5e9cf2f2a86a30e244d1aa75dac433cf713b9247; tab=system; sid-6=88dc79bb144c15ada88f9188d0b2a7f9; sid-5=0a67c6c03a8ea3b685bd6ed7a25e2724; sid-18=dfd588e674f203cb90dbe84b95979d85; sid=dfd588e674f203cb90dbe84b95979d85"""
 def get(interval=0.5):
-    url = '''     http://192.168.1.147/zentao/my-team.html?zin=1'''
+    url = '''http://192.168.1.147/zentao/index-index-L3plbnRhby9teS10ZWFtLWlkX2Rlc2MtMTktMTUtMS5odG1s.html'''
     headers = {
-        'cookie': """lang=zh-cn; vision=rnd; device=desktop; keepLogin=on; hideMenu=false; preProductID=1; preBranch=0; za=wangliping; lastProject=1; zentaosid=2a3bc5708216fb6f63e83ea4ed6ed2c9; zp=c6294800914b7e84094b44465ac40ecd007a2239; theme=blue; tab=system; sid-3=9409881c68d78dd8cc2f3dbd4c587465; sid=9409881c68d78dd8cc2f3dbd4c587465""",
+        'cookie': cookie_ln,
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
         'X-Zin-Options': '{"selector":["#configJS","title>*","body>*"],"type":"list"}',
         "X-Zin-App": "system",
@@ -37,8 +37,11 @@ def get(interval=0.5):
         'Referer': 'http://192.168.1.147/zentao/index-index-L3plbnRhby9teS10ZWFtLmh0bWw=.html'
     }
     while True:
-        data = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers).json()
+        if '用户名' in response:
+            print(response)
         time.sleep(interval)
+        # print(response)
 
 
 if __name__ == "__main__":
